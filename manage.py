da1df76fb72171e83955d2de1d1179df90f697e2
GaMerBhai87 <115382072+GaMerBhai87@users.noreply.github.com>
2023-02-12 07:56:23 +0000

    Deleted images from templates folder

--- manage.py
+++ manage.py
@@ -2,7 +2,7 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
-
+from threading import Thread
 
 def main():
     """Run administrative tasks."""
@@ -17,6 +17,10 @@ def main():
         ) from exc
     execute_from_command_line(sys.argv)
 
+def keep_alive():
+  t = Thread(target=run)
+  t.start()
 
 if __name__ == '__main__':
     main()
+    keep_alive()
---
+++ static/script.js
@@ -0,0 +1,12 @@
+
+let mimo_invite = "https://getmimo.com/invite/8kbblv";
+
+let my_yt_chnl = "https://youtube.com/channel/UC6nMdm3J17YKsg8nBn_4kYw";
+
+function mimo_red() {
+  window.location.replace("https://getmimo.com/invite/8kbblv");
+}
+
+function yt_red() {
+   window.location.replace("https://youtube.com/channel/UC6nMdm3J17YKsg8nBn_4kYw");
+}
